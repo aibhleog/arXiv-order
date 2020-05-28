cba4308232550d2d3a6f2d19293a05faf89e1e64
Taylor Hutchison <aibhleog@tamu.edu>
2020-05-28 02:12:00 -0500

    looking at voting history for one post

--- plotting_total_votes_per_order.py
+++ plotting_total_votes_per_order.py
@@ -7,6 +7,8 @@ Future plans:	color points by submission window (Mon-Fri)
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from datetime import timedelta
+from datetime import datetime as dt
 
 __author__ = 'Taylor Hutchison'
 __email__ = 'aibhleog@tamu.edu'
@@ -17,10 +19,26 @@ post_df = pd.read_csv('arXiv_posts.txt',sep='\t',dtype=df_dtypes) # main table o
 df_dtypes = {'id':str,'total_votes':int,'vote_rate':str}
 votes_df = pd.read_csv('VoxCharta_voting.txt',sep='\t',dtype=df_dtypes)
 
-indexing = votes_df.query('total_votes > 0').index.values
+# -- only looking at posts over 1 week old -- #
+# ------------------------------------------- #
+times = [dt.strptime(t,'%d %b %y') for t in post_df.date.values]
+times = np.asarray(times) # useful for the sorting we'll do 
+
+# reference date of 1 week ago
+ref_date = dt.now() - timedelta(days=7)
+print(f"Looking at posts from {dt.strftime(ref_date,'%a, %d %b %y')} and before.",end='\n\n')
+
+indexing = np.arange(len(post_df)) 
+indexing = indexing[times < ref_date] # only listing entries in the dataframe that are over 1 week old
+
+old_posts = post_df.loc[indexing.tolist()].copy()
+old_votes = votes_df.loc[indexing.tolist()].copy()
+# ------------------------------------------- #
+
+indexing = old_votes.query('total_votes > 0').index.values
 
 plt.figure()
-plt.scatter(votes_df.loc[indexing,'total_votes'],post_df.loc[indexing,'order'],\
+plt.scatter(old_votes.loc[indexing,'total_votes'],old_posts.loc[indexing,'order'],\
 		edgecolor='k',alpha=0.8)
 
 ylims = plt.ylim()
