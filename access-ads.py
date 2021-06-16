baac1e253756572fe0379128f90e7f20dacdf577
Taylor Hutchison <aibhleog@tamu.edu>
2021-06-15 22:31:29 -0500

    setting up ADS script

---
+++ access-ads.py
@@ -0,0 +1,22 @@
+'''
+Script used to search ads to get citation count and any other information I want.
+
+NOTES:
+------
+It looks like I can make an API key to query the data from a terminal?
+
+the "identifier" field can be used to search by arxiv id, then there's an example
+showing how to return citation count.
+https://github.com/adsabs/adsabs-dev-api/blob/master/Search_API.ipynb
+
+'''
+
+import pandas as pd
+import numpy as np
+import sys
+
+__author__ = 'Taylor Hutchison'
+__email__ = 'aibhleog@tamu.edu'
+
+
+
